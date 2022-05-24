14a706f10d7396802168d32cf3764926f8ca0be2
Jonathas Germano <jonathas.germano@hotmail.com>
2022-05-23 21:13:46 -0300

    feat: Build Header Component complete styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,22 +2,29 @@ import Link from 'next/link'
 
 function Header() {
   return (
-    <header>
-      <div>
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+      <div className="flex items-center space-x-5">
         <Link href="/">
           <img
             className="w-44 cursor-pointer object-contain"
             src="https://links.papareact.com/yvf"
           />
         </Link>
-        <div className="">
+        <div className="hidden items-center space-x-5 md:inline-flex">
           <h3>About</h3>
           <h3>Contact</h3>
-          <h3>Follow</h3>
+          <h3 className="rounded-full bg-green-600 px-4 py-1 text-white">
+            Follow
+          </h3>
         </div>
       </div>
 
-      <div></div>
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sign In</h3>
+        <h3 className="rounded-full border border-green-600 px-4 py-1">
+          Get Started
+        </h3>
+      </div>
     </header>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Header from '../components/Header'
 
 const Home = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
